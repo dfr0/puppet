a7bac086981c4e24b9023ae9726c8041a7eae02e
frodorik21@gmail.com <frodorik21@gmail.com>
2017-05-31 11:55:37 +0200

    agrego el usuario david

--- modules/localusers/manifests/init.pp
+++ modules/localusers/manifests/init.pp
@@ -1,4 +1,11 @@
 class localusers {
+	user { 'david':
+		ensure     => present,
+		uid        => '603',
+		shell      => '/bin/bash',
+		home       => '/home/david',
+		gid        => 'wheel',
+		managehome => true,
+		password   => '$6$k7McrV/E$mXuSYQFk2zbeY0lRnCpx2b.mCsbGWvl//qX0GQx4Z2erbSs./tpXoUqoAGwufMzfod71zcs1Mgm09x.gm65yU1',
+	}
 }
-
-
